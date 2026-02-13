94139492c6bfc801afeedbaef34edaa443a8ed41
clintonsteiner <clintonsteiner@gmail.com>
2026-02-13 00:38:31 -0600

    docs: updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,15 +2,20 @@
 
 import os
 import sys
+import tomllib
+from pathlib import Path
 
 # Add the project root to the path so autodoc can find the modules
 sys.path.insert(0, os.path.abspath("../.."))
+pyproject_file = Path(__file__).parent.parent.parent / "pyproject.toml"
 
 # -- Project information -----------------------------------------------------
-project = "Hildie"
-copyright = "2026, Clinton Steiner"
-author = "Clinton Steiner"
-release = "0.2.21"
+with open(pyproject_file, "rb") as f:
+    project_metadata = tomllib.load(f)
+
+# -- Project information -----------------------------------------------------
+project = project_metadata["project"]["name"]
+author = ", ".join([a["name"] for a in project_metadata["project"]["authors"]])
 
 # -- General configuration ---------------------------------------------------
 extensions = [
@@ -42,6 +47,14 @@ html_theme_options = {
     "navigation_depth": 4,
     "includehidden": True,
     "titles_only": False,
+}
+
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "clintonsteiner",  # Username
+    "github_repo": "hildies-python-monorepo",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/docs/source/",
 }
 
 # -- Intersphinx configuration -----------------------------------------------
